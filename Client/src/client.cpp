dd3ce2b161b62ff6ed76f95f959950269c71b304
Luan Corrionero <luancorrionero123@gmail.com>
2023-05-19 17:03:16 -0300

    build(common): se agrega libreria yaml
    Se agrega la libreria yaml, útil para manejar archivos .yaml en c++.
    Los archivos .yaml son fáciles de leer y modificar manualmente. Pueden
    ser utilizados para configuraciones en el juego.
    
    También se modifica el pre-commit. Ahora ignora la carpeta libs.
    Esto para evitar errores y warns de los chequeos en archivos que
    no son nuestros, sino de las librerias importadas.
    
    Por último se modifica el main de Client para probar yaml.

--- Client/src/client.cpp
+++ Client/src/client.cpp
@@ -1,5 +1,6 @@
 #include <exception>
 #include <SDL2pp/SDL2pp.hh>
+#include "yaml-cpp/yaml.h"
 #include "../include/client.h"
 
 Client::Client(const char *hostname, const char *servname) {
@@ -7,5 +8,7 @@ Client::Client(const char *hostname, const char *servname) {
 
 void Client::init() {
     SDL2pp::SDL sdl(SDL_INIT_VIDEO);
-
+    YAML::Emitter out;
+    out << "Hello world";
+    std::cout << "YAML out: " << out.c_str() << std::endl;
 }

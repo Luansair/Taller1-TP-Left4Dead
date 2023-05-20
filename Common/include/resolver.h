816419b0715e56a6daedb7fcb92478330f206cb8
Luan Corrionero <luancorrionero123@gmail.com>
2023-05-20 18:42:27 -0300

    feat(common): se declara e implementa Resolver
    Resolver resuelve direcciones según un hostname, un servname y un booleano indicando si es pasivo (para servidores) o no (para clientes).
    Las direcciones utilizan el protocolo TCP.
    Lanza excepciones del tipo runtime ante cualquier error.
    
    No tiene pruebas unitarias.

---
+++ Common/include/resolver.h
@@ -0,0 +1,27 @@
+#ifndef RESOLVER_H_
+#define RESOLVER_H_
+
+#include <string>
+
+class Resolver {
+    struct addrinfo* first;
+    struct addrinfo* next;
+
+public:
+    Resolver(
+            const char* hostname,
+            const char* servname,
+            bool is_passive);
+
+    Resolver(const Resolver&) = delete;
+    Resolver& operator=(const Resolver&) = delete;
+
+    Resolver(Resolver&&) noexcept;
+    Resolver& operator=(Resolver&&);
+
+    bool hasNext();
+
+    struct addrinfo* nextAddr();
+};
+
+#endif  // RESOLVER_H_
---
+++ Common/src/resolver.cpp
@@ -0,0 +1,64 @@
+#include <netdb.h>
+#include <cstring>
+#include <stdexcept>
+#include "../include/resolver.h"
+
+Resolver::Resolver(const char *hostname,
+                   const char *servname,
+                   bool is_passive) {
+    struct addrinfo hints{};
+    first = next = nullptr;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = is_passive ? AI_PASSIVE : 0;
+
+    int ret = getaddrinfo(hostname, servname, &hints, &this->first);
+
+    if (ret != 0) {
+        if (ret == EAI_SYSTEM) {
+            throw std::runtime_error("Name resolution failed.\n");
+        } else {
+            throw std::runtime_error(gai_strerror(ret));
+        }
+    }
+    next = first;
+}
+
+Resolver::Resolver(Resolver&& other)  noexcept {
+    first = other.first;
+    next = other.next;
+    other.first = nullptr;
+    other.next = nullptr;
+}
+
+Resolver& Resolver::operator=(Resolver&& other) {
+    if (this == &other)
+        return *this;
+
+    if (first)
+        freeaddrinfo(first);
+
+    first = other.first;
+    next = other.next;
+    other.first = nullptr;
+    other.next = nullptr;
+
+    return *this;
+}
+
+bool Resolver::hasNext() {
+    return next != nullptr;
+}
+
+struct addrinfo* Resolver::nextAddr() {
+    if (!hasNext()) {
+        throw std::runtime_error("Error: Attempt to retrieve next address from Resolver when none exists. "
+                                 "Remember to check if Resolver has more addresses with 'hasNext()' method. "
+                                 "This is to avoid infinite loops.");
+    }
+    struct addrinfo* actual = next;
+    next = actual->ai_next;
+    return actual;
+}

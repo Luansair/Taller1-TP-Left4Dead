fc7f5ba4f11dac86099e2501a305c238246397e3
Luan Corrionero <luancorrionero123@gmail.com>
2023-05-20 23:56:56 -0300

    feat(common): se define la clase Socket y sus metodos
    Socket es una clase común para Client y Server que se encarga de enviar y recibir datos.
    Utiliza un Resolver para resolver direcciones y obtener un fd a partir de una de ellas.
    Se implementa solo el constructor para cliente.

--- Common/include/socket.h
+++ Common/include/socket.h
@@ -1,8 +1,38 @@
-//
-// Created by luan on 18/05/23.
-//
+#ifndef SOCKET_H_
+#define SOCKET_H_
 
-#ifndef TALLER1_TP_LEFT4DEAD_SOCKET_H
-#define TALLER1_TP_LEFT4DEAD_SOCKET_H
+#include <cstdint>
 
-#endif //TALLER1_TP_LEFT4DEAD_SOCKET_H
+class Socket {
+    int fd;
+    bool closed;
+
+    explicit Socket(int sktfd);
+
+    std::size_t sendSome(std::int8_t* data, std::size_t amount);
+    std::size_t recvSome(std::int8_t* data, std::size_t amount);
+
+public:
+    Socket(const char *hostname, const char *servname);
+
+    explicit Socket(const char *servname);
+
+    Socket(const Socket&) = delete;
+    Socket& operator=(const Socket&) = delete;
+
+    Socket(Socket&&) noexcept;
+    Socket& operator=(Socket&&);
+
+    void send(std::int8_t* data, std::size_t amount);
+    void recv(std::int8_t* data, std::size_t amount);
+
+    Socket accept() const;
+
+    void shutdown(int how) const;
+
+    int close();
+
+    ~Socket();
+};
+
+#endif  // SOCKET_H_

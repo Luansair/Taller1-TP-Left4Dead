e68a2e375af6ddad155aaf8ec4a0fb2d9445d282
pgallino <pedrogallino11@gmail.com>
2023-05-25 21:06:27 -0300

    feat(server): declaración e "implementacion" de comandos
    Con los patrones de diseño command y factory se declaran los comandos del juego.
    La clase factory recibe el id de action y el comando correspondiente.
    Cada comando recibe la data (action->serializar) recibida en receiver y el id_jugador.
    Luego se declara el método execute, recibe por referencia al game para poder indicarle como modificar el juego dependiendo del comando.

--- Server/include/command.h
+++ Server/include/command.h
@@ -1,8 +1,89 @@
 #ifndef COMMAND_H_
 #define COMMAND_H_
 
-class Command {
+#include "game.h"
 
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Command {
+    public:
+        virtual ~Command() {
+        }
+        virtual void Execute() const = 0;
+};
+
+class ShootCommand : public Command {
+private:
+    uint_8 player_id_;
+    vector<int8_t> data;
+
+public:
+    explicit ShootCommand(uint_8 player_id, vector<int8_t> data);
+
+    void Execute(Game &game) const override;
+};
+
+class IdleCommand : public Command {
+private:
+    uint_8 player_id_;
+    vector<int8_t> data;
+
+public:
+    explicit IdleCommand(uint_8 player_id, vector<int8_t> data);
+
+    void Execute(Game &game) const override;
+};
+
+class MoveCommand : public Command {
+private:
+    uint_8 player_id_;
+    vector<int8_t> data;
+
+public:
+    explicit MoveCommand(uint_8 player_id, vector<int8_t> data);
+
+    void Execute(Game &game) const override;
+};
+
+class CgrenadeCommand : public Command {
+private:
+    uint_8 player_id_;
+    vector<int8_t> data;
+
+public:
+    explicit CgrenadeCommand(uint_8 player_id, vector<int8_t> data);
+
+    void Execute(Game &game) const override;
+};
+
+class ReloadCommand : public Command {
+private:
+    uint_8 player_id_;
+    vector<int8_t> data;
+
+public:
+    explicit ReloadCommand(uint_8 player_id, vector<int8_t> data);
+
+    void Execute(Game &game) const override;
+};
+
+class ThrowCommand : public Command {
+private:
+    uint_8 player_id_;
+    vector<int8_t> data;
+
+public:
+    explicit ThrowCommand(uint_8 player_id, vector<int8_t> data);
+
+    void Execute(Game &game) const override;
+};
+
+class Factory {
+public:
+    Command* create(Game &game, uint_8 player_id, vector<int8_t> data);
 };
 
 #endif  // COMMAND_H_
--- Server/src/command.cpp
+++ Server/src/command.cpp
@@ -1,3 +1,69 @@
-//
-// Created by luan on 18/05/23.
-//
+#include "../include/command.h"
+
+ShootCommand::ShootCommand(uint_8 player_id, vector<int8_t> data) : 
+    player_id_(player_id),
+    data_(data) {
+    }
+
+void ShootCommand::Execute(Game &game) const {
+    // int8_t state = data[1];
+    // game.shoot(player_id_, state);
+}
+
+MoveCommand::MoveCommand(uint_8 player_id, vector<int8_t> data) : 
+    player_id_(player_id),
+    data_(data) {
+    }
+
+void MoveCommand::Execute(Game &game) const {
+}
+
+IdleCommand::IdleCommand(uint_8 player_id, vector<int8_t> data) : 
+    player_id_(player_id),
+    data_(data) {
+    }
+
+void IdleCommand::Execute(Game &game) const {
+}
+
+ReloadCommand::ReloadCommand(uint_8 player_id, vector<int8_t> data) : 
+    player_id_(player_id),
+    data_(data) {
+    }
+
+void ReloadCommand::Execute(Game &game) const {
+}
+
+CgrenadeCommand::CgrenadeCommand(uint_8 player_id, vector<int8_t> data) : 
+    player_id_(player_id),
+    data_(data) {
+    }
+
+void CgrenadeCommand::Execute(Game &game) const {
+}
+
+ThrowCommand::ThrowCommand(uint_8 player_id, vector<int8_t> data) : 
+    player_id_(player_id),
+    data_(data) {
+    }
+
+void ThrowCommand::Execute(Game &game) const {
+}
+
+Command* Factory::create(Game &game, uint_8 player_id, vector<int8_t> data) {
+    switch(data[0])
+    {
+        case IDLE:
+            return new IdleCommand(game, player_id, data);
+        case SHOOT:
+            return new ShootCommand(game, player_id, data);
+        case MOVE:
+            return new MoveCommand(game, player_id, data);
+        case RELOAD:
+            return new ReloadCommand(game, player_id, data);
+        case CHANGE_GRENADE:
+            return new CgrenadeCommand(game, player_id, data);
+        case THROW:
+            return new ThrowCommand(game, player_id, data);
+    }
+}

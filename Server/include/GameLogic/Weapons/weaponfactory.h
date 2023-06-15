b5236b1e1c67e9ef8975b63dca72a0b658cd7ec4
pgallino <pedrogallino11@gmail.com>
2023-06-14 23:38:07 -0300

    fix(Server): Seguimos debuggeando, saco yaml de factory
    Seguimos debuggeando saco yaml de factory

--- Server/include/GameLogic/Weapons/weaponfactory.h
+++ Server/include/GameLogic/Weapons/weaponfactory.h
@@ -12,6 +12,19 @@
 #define SCOUTWEAPON 2
 #define IDFWEAPON 3
 
+#define P90_DAMAGE 50
+#define P90_AMMO 30
+#define P90_SCOPE 2
+#define P90_REDUCTION 0.8
+#define SCOUT_DAMAGE 60
+#define SCOUT_AMMO 20
+#define SCOUT_SCOPE 2
+#define SCOUT_REDUCTION 0.8
+#define IDF_DAMAGE 80
+#define IDF_AMMO 50
+#define IDF_SCOPE 2
+#define IDF_REDUCTION 0.8
+
 class WeaponFactory {
 public:
     std::unique_ptr<Weapon> create(uint8_t weapon_type);

5656dab1687d7883c776f47be143c6d2baaebc40
luansair <lcorrionero@fi.uba.ar>
2023-06-26 01:19:05 -0300

    feat(ui): se termina de implementar el lobby.
    El lobby lanza una Qapp para que el usuario interactue. Se puede unir y crear partidas.
    En lobbywindow.cpp esta toda la logica donde se pushean las request y se popea el feedback.
    
    Se soluciona un error en el server que no enviaba feedback sobre el join (si fue exitoso o no).

--- Client/src/LobbyUI/lobbywindow.h
+++ Client/src/LobbyUI/lobbywindow.h
@@ -28,10 +28,40 @@ private slots:
 
     void on_pushButton_joingame_clicked();
 
+    void on_pushButton_p90soldier_clicked();
+
+    void on_lineEdit_gamecode_textChanged(const QString &arg1);
+
+    void on_pushButton_idfsoldier_clicked();
+
+    void on_pushButton_scoutsoldier_clicked();
+
+    void on_pushButton_survival_clicked();
+
+    void on_pushButton_clearthezone_clicked();
+
+    void on_pushButton_easy_clicked();
+
+    void on_pushButton_normal_clicked();
+
+    void on_pushButton_hard_clicked();
+
+    void on_pushButton_insane_clicked();
+
+    void on_pushButton_clicked();
+
 private:
     Queue<std::shared_ptr<Information>>& actions_to_send;
     Queue<std::shared_ptr<Information>>& feedback_received;
+    std::uint8_t game_type;
+    std::uint8_t game_difficulty;
+    std::uint8_t soldier_type;
+    QPalette red_palette;
+    QPalette green_palette;
+    QPalette white_palette;
     Ui::LobbyWindow *ui;
+
+    void create_game_process();
 };
 
 #endif // LOBBYWINDOW_H
